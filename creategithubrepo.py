b4909ff28285c372fae5cc213ec3d582cc13d8cf
erichall7575 <48109025+erichall7575@users.noreply.github.com>
2019-03-01 14:58:50 -0500

    Add files via upload

--- creategithubrepo.py
+++ creategithubrepo.py
@@ -1,8 +1,12 @@
 import requests,json
 import os
+import sys
 
-reponame=input("Please Enter Your New Repo Name. Must be at least 5 characters long: ")
-repodescription=input("Please Enter Your New Repo Description. Must be at least 5 characters long: ")
+reponame=sys.argv[1]
+repodescription=sys.argv[2]
+
+# reponame=input("Please Enter Your New Repo Name. Must be at least 5 characters long: ")
+# repodescription=input("Please Enter Your New Repo Description. Must be at least 5 characters long: ")
 
 if len(reponame)>5 and len(repodescription)>5: 
     github_url = "https://api.github.com/user/repos"
@@ -16,4 +20,5 @@ if len(reponame)>5 and len(repodescription)>5:
     r = requests.post(github_url, data, auth=(cred[0], cred[1]))
 
     print(r.json)
+    
 print("Invalid Repository Name or Description.")
